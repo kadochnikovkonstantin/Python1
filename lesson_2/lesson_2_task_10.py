d742799ef8e625110cbd199f657fa19f5456e110
My Name <myEmail@example.com>
2024-03-16 19:17:02 +0500

    123

--- lesson_2/lesson_2_task_10.py
+++ lesson_2/lesson_2_task_10.py
@@ -5,4 +5,8 @@ def bank(X, Y):
         X*=1.1
 
     return X
-print(bank(10,2))
+#print(bank(10,2))
+X = float(input("Введите начальную сумму вклада: "))
+Y = int(input("Введите срок вклада в годах: "))
+result = bank(X, Y)
+print("Итоговая сумма на вкладе: ", result)
