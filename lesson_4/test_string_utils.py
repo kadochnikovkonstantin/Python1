8f607a48ba04ee0ef131de8480f23698cdb7e23e
My Name <myEmail@example.com>
2024-03-28 22:38:54 +0500

    initial commit

--- lesson_4/test_string_utils.py
+++ lesson_4/test_string_utils.py
@@ -6,14 +6,30 @@ b = StringUtils()
 a = b.capitilize("skypro")
 assert a == "Skypro"
 
+a = b.capitilize("Skypro")
+assert a == "Skypro"
+
+#a = b.capitilize(None)
+#assert a == ""
+
+a = b.capitilize(" ")
+assert a == " "
+
+
 a = b.trim(" skypro")
 assert a == "skypro"
 
 a = b.to_list("a,b,c,d")
 assert a == ["a", "b", "c", "d"]
 
+#a = b.to_list(None)
+#assert a == ["a b c d"]
+
 a = b.contains("SkyPro", "S")
 assert a == True
+
+a = b.contains("SkyPro", "U")
+assert a == False
 
 a = b.delete_symbol("SkyPro", "Pro")
 assert a == "Sky" 
@@ -21,16 +37,39 @@ assert a == "Sky"
 a = b.starts_with("SkyPro", "S")
 assert a == True
 
+a = b.starts_with("", "")
+assert a == True
+
+# a = b.starts_with(" SkyPro", "S")
+# assert a == True
+
 a = b.end_with("SkyPro", "o")
+assert a == True
+
+a = b.end_with("", "")
 assert a == True
 
 a = b.is_empty(" ")
 assert a == True
 
+a = b.is_empty("")
+assert a == True
+
+a = b.is_empty("рубль")
+assert a == False
+
+#a = b.is_empty(None)
+#assert a == True
+
 a = b.list_to_string([1,2,3,4])
 assert a == "1, 2, 3, 4"
 
+a = b.list_to_string([ ])
+assert a == ""
 
+
+a = b.list_to_string("")
+assert a == ""
 
 
 """
